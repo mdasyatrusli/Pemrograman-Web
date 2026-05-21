e915179dce9283a72cb80564be1ac46b4f29aada
mdasyatrusli <130004976+mdasyatrusli@users.noreply.github.com>
2026-05-21 10:28:58 +0700

    LatihanJs

---
+++ latihan/models/BankAccount.js
@@ -0,0 +1,23 @@
+export default class BankAccount {
+  #balance = 0;
+
+  constructor(owner) {
+    this.owner = owner;
+  }
+  deposit(amount) {
+    this.#balance += amount;
+    console.log(`${this.owner} menabung Rp${amount}`);
+  }
+
+  withdraw(amount) {
+    if (amount > this.#balance) {
+      console.log(`Maaf, saldo tidak cukup`);
+      return;
+    }
+    this.#balance -= amount;
+    console.log(`${this.owner} menarik Rp${amount}`);
+  }
+  getBalance() {
+    return this.#balance;
+  }
+}
